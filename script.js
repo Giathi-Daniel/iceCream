3f3bf951cf1d2e729de5a285bcc8c15553730d77
DANIEL GIATHI <giathidaniel252@gmail.com>
2024-01-08 11:45:03 +0300

    iceCream

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+//PAGINATION ANIMATION
 var swiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
     },
+});
+
+//ICE TOP 
+document.querySelector('.top-image').forEach(image_1 => {
+    image_1.addEventListener('click',() => {
+        var src = image_1.getAttribute('src');
+        document.querySelector('.big-image').src = src;
+    })
 });
